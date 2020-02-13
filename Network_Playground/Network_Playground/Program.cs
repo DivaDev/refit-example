b04d972ae54fa60a87bf5693412056ba40d13d8c
DivaDev <fabulousplatypus@gmail.com>
2020-02-13 15:48:51 -0700

    Initial project commit

---
+++ Network_Playground/Network_Playground/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Refit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Network_Playground
+{
+    public interface ICatsApi
+    {
+        [Get("/v1/images/search?breed_id=[id]")]
+        Task<Cats> GetCats(string myId);
+
+    }
+    public class Cats
+    {
+        //[JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+
+        //[JsonProperty(PropertyName = "width")]
+        public int width { get; set; }
+
+        //[JsonProperty(PropertyName = "height")]
+        public int height { get; set; }
+
+        //[JsonProperty(PropertyName = "breeds")]
+        //public B breeds { get; set; }
+
+        //[JsonProperty(PropertyName = "url")]
+        public string url { get; set; }
+    }
+
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var catsApi = RestService.For<ICatsApi>("https://api.thecatapi.com");
+            var cats = await catsApi.GetCats("beng");
+            Console.WriteLine(cats);
+            //Console.WriteLine($" id:{cats.id}, name:{cats.height}, origin:{cats.width}");
+        }
+    }
+}
