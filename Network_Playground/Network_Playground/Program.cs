b0550d41a5fce744d7ab0498c1dd70d7dc206275
Laurel Stewart <laurel.stewart@p86547.local>
2020-02-14 17:11:44 -0700

    Fixed error getting data from API

--- Network_Playground/Network_Playground/Program.cs
+++ Network_Playground/Network_Playground/Program.cs
@@ -5,13 +5,26 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 namespace Network_Playground
 {
+    /*
+     * [
+	{
+      "breeds":
+		[ list of attributes ],
+	 "id":"LSaDk6OjY",
+      "url":"https://cdn2.thecatapi.com/images/LSaDk6OjY.jpg",
+      "width":1080,
+      "height":1080
+     }
+        ]
+     */
     public interface ICatsApi
     {
-        [Get("/v1/images/search?breed_id=[id]")]
-        Task<Cats> GetCats(string myId);
+        [Get("/v1/images/search?breed_id={id}")]
+        Task<List<Cat>> GetCats(string id);
 
     }
-    public class Cats
+
+    public class Cat
     {
         //[JsonProperty(PropertyName = "id")]
         public string id { get; set; }
@@ -29,14 +42,19 @@ namespace Network_Playground
         public string url { get; set; }
     }
 
+    public class Herd
+    {
+        public List<Cat> breeds { get; set;}
+    }
+
     class Program
     {
         public static async Task Main(string[] args)
         {
             var catsApi = RestService.For<ICatsApi>("https://api.thecatapi.com");
             var cats = await catsApi.GetCats("beng");
-            Console.WriteLine(cats);
-            //Console.WriteLine($" id:{cats.id}, name:{cats.height}, origin:{cats.width}");
+           // Console.WriteLine(cats);
+            Console.WriteLine($" id:{cats[0].id}, name:{cats[0].height}, origin:{cats[0].width}");
         }
     }
 }
